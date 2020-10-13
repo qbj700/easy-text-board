dcb033b56ac20a148b06f3b6d948acaafc6598dc
qbj700 <qbj700@gmail.com>
2020-10-13 13:10:53 +0900

    게시물 2개까지 저장 할 수 있는 기능 구현

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -6,29 +6,65 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		int LastArticleid = 0;
+		int lastArticleid = 0;
+
+		// 1번 게시물 저장소
+		int article1__id = 0;
+		String article1__title = "";
+		String article1__body = "";
+
+		// 2번 게시물 저장소
+		int article2__id = 0;
+		String article2__title = "";
+		String article2__body = "";
+
 		while (true) {
 			System.out.printf("명령어 입력 :");
 			String command = scanner.nextLine();
+			// System.out.println(command);
+
+			int id = lastArticleid + 1;
+			String title;
+			String body;
 
 			if (command.equals("article add")) {
-				System.out.println("== 게시물 작성 ==");
-
+				System.out.println("== 게시물 등록 ==");
 				System.out.printf("제목 :");
-				String title = scanner.nextLine();
+				title = scanner.nextLine();
 				System.out.printf("내용 :");
-				String body = scanner.nextLine();
-				int id = LastArticleid + 1;
+				body = scanner.nextLine();
 
-				System.out.println("== 생성된 게시물 정보 ==");
-				System.out.printf("번호 : %d\n", id);
+				if (id == 1) {
+					article1__id = id;
+					article1__title = title;
+					article1__body = body;
+				} else if (id == 2) {
+					article2__id = id;
+					article2__title = title;
+					article2__body = body;
+				}
+
+				System.out.println("== 게시물 등록 결과 ==");
 				System.out.printf("제목 : %s\n", title);
 				System.out.printf("내용 : %s\n", body);
-				
-				LastArticleid = id;
+				System.out.printf("%s번 게시물이 생성되었습니다\n", id);
 
+				// 가장 마지막 게시물 번호를 갱신
+				lastArticleid = id;
+				
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
+				if (lastArticleid == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
+				if (lastArticleid >= 1) {
+					System.out.println("번호 / 제목");
+					System.out.printf("%d / %s\n", article1__id, article1__title);
+				}
+				if (lastArticleid >= 2) {
+					System.out.printf("%d / %s\n", article2__id, article2__title);
+				}
 			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
