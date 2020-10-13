97034aa9a1b7c3e80e4f81b4e72308d0e92b27f0
qbj700 <qbj700@gmail.com>
2020-10-13 10:01:24 +0900

    게시물 작성시 번호가 자동으로 증가하도록 처리

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		int id = 1;
+		int LastArticleid = 0;
 		while (true) {
 			System.out.printf("명령어 입력 :");
 			String command = scanner.nextLine();
@@ -18,12 +18,14 @@ public class Main {
 				String title = scanner.nextLine();
 				System.out.printf("내용 :");
 				String body = scanner.nextLine();
+				int id = LastArticleid + 1;
 
 				System.out.println("== 생성된 게시물 정보 ==");
-				System.out.println("번호 : " + id);
-				System.out.println("제목 : " + title);
-				System.out.println("내용 : " + body);
-				id = id + 1;
+				System.out.printf("번호 : %d\n", id);
+				System.out.printf("제목 : %s\n", title);
+				System.out.printf("내용 : %s\n", body);
+				
+				LastArticleid = id;
 
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
@@ -31,7 +33,7 @@ public class Main {
 				System.out.println("== 프로그램 종료 ==");
 				break;
 			} else {
-				System.out.println("== 존재하지 않는 명령어 =");
+				System.out.println("== 존재하지 않는 명령어 ==");
 			}
 		}
 		scanner.close();
