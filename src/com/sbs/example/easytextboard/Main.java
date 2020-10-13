21d6ae4a2f833eb48fbd0c1c254a20b64260f3dc
qbj700 <qbj700@gmail.com>
2020-10-13 09:43:27 +0900

    고객이 정보를 입력하여 게시물 작성하는 코드 구현

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -6,19 +6,32 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		int id = 1;
 		while (true) {
-			System.out.printf("명령어 입력:");
+			System.out.printf("명령어 입력 :");
 			String command = scanner.nextLine();
 
 			if (command.equals("article add")) {
-				System.out.println("== 게시물 등록 ==");
+				System.out.println("== 게시물 작성 ==");
+
+				System.out.printf("제목 :");
+				String title = scanner.nextLine();
+				System.out.printf("내용 :");
+				String body = scanner.nextLine();
+
+				System.out.println("== 생성된 게시물 정보 ==");
+				System.out.println("번호 : " + id);
+				System.out.println("제목 : " + title);
+				System.out.println("내용 : " + body);
+				id = id + 1;
+
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
 			} else {
-				System.out.println("== 존재하지 않는 명령어 ==");
+				System.out.println("== 존재하지 않는 명령어 =");
 			}
 		}
 		scanner.close();
