2e7c6f5355dafcfd7468b61538925702a7544bea
qbj700 <qbj700@gmail.com>
2020-10-16 11:12:21 +0900

    전체 소스코드 재구현 if문을 ==> for문으로

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class App {
 	// 1번 게시물 저장소
 	Article article1 = new Article();
-
 	// 2번 게시물 저장소
 	Article article2 = new Article();
+	
 
 	public Article getArticle(int id) {
 		if (id == 1) {
@@ -21,6 +21,7 @@ public class App {
 	public void run() {
 		Scanner scanner = new Scanner(System.in);
 
+		int maxArticlesCount = 10;
 		int lastArticleid = 0;
 
 		while (true) {
@@ -30,6 +31,11 @@ public class App {
 
 			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
+
+				if (lastArticleid >= maxArticlesCount) {
+					System.out.println("더 이상 생성할 수 없습니다.");
+					continue;
+				}
 
 				int id = lastArticleid + 1;
 				String title;
@@ -61,33 +67,41 @@ public class App {
 				System.out.println("== 게시물 리스트 ==");
 
 				if (lastArticleid == 0) {
-					System.out.println("게시물이 없습니다.");
+					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
-				if (lastArticleid >= 1) {
-					System.out.println("번호 / 제목");
-					System.out.printf("%d / %s\n", article1.id, article1.title);
-				}
-				if (lastArticleid >= 2) {
-					System.out.printf("%d / %s\n", article2.id, article2.title);
+				System.out.println("번호 / 제목");
+
+				for (int i = 1; i <= lastArticleid; i++) {
+					Article article = null;
+
+					if (i == 1) {
+						article = article1;
+					}
+					if (i == 2) {
+						article = article2;
+					}
+
+					System.out.printf("%d / %s\n", article.id, article.title);
 				}
 
 			} else if (command.startsWith("article detail ")) {
 				int inputedid = Integer.parseInt(command.split(" ")[2]);
 
 				System.out.println("== 게시물 상세 ==");
-				
-				Article selectedArticle = getArticle(inputedid);
-				
-				if (selectedArticle == null || selectedArticle.id == 0) {
+
+				if (lastArticleid == 0 || inputedid > lastArticleid) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedid);
 					continue;
 				}
-				System.out.printf("번호 : %d\n", selectedArticle.id);
-				System.out.printf("제목 : %s\n", selectedArticle.title);
-				System.out.printf("내용 : %s\n", selectedArticle.body);
 
-			} else if (command.equals("article exit")) {
+				Article article = getArticle(inputedid);
+
+				System.out.printf("번호 : %d\n", article.id);
+				System.out.printf("제목 : %s\n", article.title);
+				System.out.printf("내용 : %s\n", article.body);
+
+			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
 			} else {
