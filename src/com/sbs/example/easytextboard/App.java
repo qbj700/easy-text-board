c8416e8f59f1e5a083c3b55ca78b7e1f7536a67f
qbj700 <qbj700@gmail.com>
2020-10-20 15:12:49 +0900

    0번 게시물 상세보기 시 종료되는 버그 수정

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -4,28 +4,32 @@ import java.util.Scanner;
 
 public class App {
 
-	Article[] articles = new Article[100];
+	Article[] articles = new Article[5];
+	int lastArticleId = 0;
 
 	Article getArticle(int id) {
-		return articles[id -1];
+		if (id < 1) {
+			return null;
+		}
+		if (id > lastArticleId) {
+			return null;
+		}
+		return articles[id - 1];
 	}
 
 	public void run() {
-		Scanner scanner = new Scanner(System.in);
-		
 		for (int i = 0; i < articles.length; i++) {
 			articles[i] = new Article();
 		}
 
-		int lastArticleId = 0;
+		Scanner sc = new Scanner(System.in);
+
 		int maxArticlesConunt = articles.length;
 
 		while (true) {
 
 			System.out.printf("명령어 입력 :");
-			String command = scanner.nextLine();
-
-			int id = lastArticleId + 1;
+			String command = sc.nextLine();
 
 			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
@@ -35,23 +39,25 @@ public class App {
 					continue;
 				}
 
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+
 				System.out.printf("제목 :");
-				String title = scanner.nextLine();
+				String title = sc.nextLine();
 				System.out.printf("내용 :");
-				String body = scanner.nextLine();
+				String body = sc.nextLine();
 
 				System.out.println("== 게시물 등록 결과 ==");
 				System.out.printf("제목 :%s\n", title);
 				System.out.printf("내용 :%s\n", body);
-				System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
 
 				Article article = getArticle(id);
+
 				article.id = id;
 				article.title = title;
 				article.body = body;
 
-				lastArticleId = id;
-
+				System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 
@@ -60,21 +66,23 @@ public class App {
 					continue;
 				}
 				System.out.println("번호 / 제목");
+
 				for (int i = 1; i <= lastArticleId; i++) {
 					Article article = getArticle(i);
+
 					System.out.printf("%d / %s\n", article.id, article.title);
 				}
-
 			} else if (command.startsWith("article detail ")) {
 				int inputedid = Integer.parseInt(command.split(" ")[2]);
 				System.out.println("== 게시물 상세 ==");
 
-				if (lastArticleId == 0 || inputedid > lastArticleId) {
+				Article article = getArticle(inputedid);
+
+				if (article == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedid);
 					continue;
 				}
-				
-				Article article = getArticle(inputedid);
+
 				System.out.printf("번호 : %d\n", article.id);
 				System.out.printf("제목 : %s\n", article.title);
 				System.out.printf("내용 : %s\n", article.body);
@@ -86,6 +94,6 @@ public class App {
 				System.out.println("== 존재하지 않는 명령어 입니다. ==");
 			}
 		}
-		scanner.close();
+		sc.close();
 	}
 }
