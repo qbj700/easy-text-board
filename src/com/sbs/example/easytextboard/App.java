d8369ade37e94c10d706790fce3d1f29fd936192
qbj700 <qbj700@gmail.com>
2020-10-21 12:36:08 +0900

    게시물 수정 기능 추가(연습)

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class App {
 
 	Article[] articles = new Article[5];
+
 	int lastArticleId = 0;
 	int articlesSize = 0;
 
@@ -21,10 +22,30 @@ public class App {
 		return articles[index];
 	}
 
+	private int getIndexById(int id) {
+		for (int i = 0; i < articlesSize(); i++) {
+			if (articles[i].id == id) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private void remove(int id) {
+		int index = getIndexById(id);
+
+		if (index == -1) {
+			return;
+		}
+		for (int i = index + 1; i < articlesSize(); i++) {
+			articles[i - 1] = articles[i];
+		}
+		articlesSize--;
+	}
+	
 	public void run() {
 		Scanner sc = new Scanner(System.in);
-
-		int maxArticlesConunt = articles.length;
+		int maxArticlesCount = articles.length;
 
 		while (true) {
 
@@ -34,8 +55,8 @@ public class App {
 			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
 
-				if (articlesSize() >= maxArticlesConunt) {
-					System.out.println("더 이상 생성할 수 없습니다.");
+				if (articlesSize() >= maxArticlesCount) {
+					System.out.println("더 이상 등록할 수 없습니다.");
 					continue;
 				}
 
@@ -52,12 +73,11 @@ public class App {
 				System.out.printf("내용 :%s\n", body);
 
 				Article article = new Article();
-
 				article.id = id;
 				article.title = title;
 				article.body = body;
 
-				System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+				System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
 
 				articles[articlesSize] = article;
 
@@ -71,12 +91,11 @@ public class App {
 					continue;
 				}
 				System.out.println("번호 / 제목");
-
-				for (int i = 0; i < articlesSize(); i++) {
+				for (int i = 0; i < articlesSize; i++) {
 					Article article = articles[i];
-
 					System.out.printf("%d / %s\n", article.id, article.title);
 				}
+
 			} else if (command.startsWith("article detail ")) {
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
 				System.out.println("== 게시물 상세 ==");
@@ -102,39 +121,37 @@ public class App {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
 					continue;
 				}
-
 				remove(inputedId);
-
 				System.out.printf("%d번 게시물이 삭제되었습니다.\n", inputedId);
+
+			} else if (command.startsWith("article modify ")) {
+				int inputedId = Integer.parseInt(command.split(" ")[2]);
+				System.out.println("== 게시물 수정 ==");
+
+				Article article = getArticle(inputedId);
+
+				if (article == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
+					continue;
+				} else {
+					System.out.printf("수정할 제목 :");
+					String title = sc.nextLine();
+					System.out.printf("수정할 내용 :");
+					String body = sc.nextLine();
+					
+					article.id = inputedId;
+					article.title = title;
+					article.body = body;
+					System.out.printf("%d번 게시물이 수정되었습니다.\n", inputedId);
+				}
 
 			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
 			} else {
-				System.out.println("== 존재하지 않는 명령어 입니다. ==");
+				System.out.println("== 존재하지 않는 명령어 입니다  ==");
 			}
 		}
 		sc.close();
-	}
-
-	private void remove(int id) {
-		int index = getIndexById(id);
-
-		if (index == -1) {
-			return;
-		}
-		for (int i = index + 1; i < articlesSize(); i++) {
-			articles[i - 1] = articles[i];
-		}
-		articlesSize--;
-	}
-
-	private int getIndexById(int id) {
-		for (int i = 0; i < articlesSize(); i++) {
-			if (articles[i].id == id) {
-				return i;
-			}
-		}
-		return -1;
 	}
 }
