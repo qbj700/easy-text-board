21a11a9a7df1daa81e6a926164b4d45f0320d06c
qbj700 <qbj700@gmail.com>
2020-10-21 11:06:31 +0900

    게시물 개수 구하는 함수를 만들어서 전체 소스코드에 적용

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -6,7 +6,12 @@ public class App {
 
 	Article[] articles = new Article[5];
 	int lastArticleId = 0;
+	int articlesSize = 0;
 
+	int articlesSize() {
+		return articlesSize;
+	}
+	
 	Article getArticle(int id) {
 		if (id < 1) {
 			return null;
@@ -34,7 +39,7 @@ public class App {
 			if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
 
-				if (lastArticleId >= maxArticlesConunt) {
+				if (articlesSize() >= maxArticlesConunt) {
 					System.out.println("더 이상 생성할 수 없습니다.");
 					continue;
 				}
@@ -58,10 +63,13 @@ public class App {
 				article.body = body;
 
 				System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+				
+				articlesSize++;
+				
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 
-				if (lastArticleId == 0) {
+				if (articlesSize() == 0) {
 					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
