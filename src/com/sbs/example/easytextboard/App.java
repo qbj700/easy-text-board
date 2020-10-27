e76fc60293bdc923ca920651f62a22ced3723fe2
qbj700 <qbj700@gmail.com>
2020-10-27 12:45:00 +0900

    회원가입, 로그인, 로그인정보, 로그아웃 기능 구현 (연습)

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -7,11 +7,16 @@ import java.util.Scanner;
 public class App {
 
 	private List<Article> articles;
+	private List<Member> members;
 	private int lastArticleId;
+	private int lastMemberCount;
+	private int loginInformation;
 
 	public App() {
 		articles = new ArrayList<>();
+		members = new ArrayList<>();
 		lastArticleId = 0;
+		loginInformation = -1;
 
 		for (int i = 0; i < 32; i++) {
 			add("제목" + (i + 1), "내용" + (i + 1));
@@ -69,6 +74,34 @@ public class App {
 		Article article = getArticle(inputedId);
 		article.title = title;
 		article.body = body;
+	}
+
+	// 회원 로그인 함수
+	private int login(String id, String pw) {
+		for (int i = 0; i < members.size(); i++) {
+			if (members.get(i).id.equals(id)) {
+				if (members.get(i).pw.equals(pw)) {
+					loginInformation = i;
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
+	// 회원 가입 함수
+	private int join(String name, String id, String pw) {
+
+		Member member = new Member();
+		member.count = lastMemberCount + 1;
+		member.name = name;
+		member.id = id;
+		member.pw = pw;
+
+		members.add(member);
+		lastMemberCount = member.count;
+
+		return member.count;
 	}
 
 	public void run() {
@@ -260,6 +293,58 @@ public class App {
 
 					System.out.printf("%d / %s\n", article.id, article.title);
 				}
+
+			} else if (command.equals("member join")) {
+				System.out.println("== 회원 가입 ==");
+
+				System.out.printf("이름을 입력하세요 :");
+				String name = sc.nextLine();
+				System.out.printf("로그인 ID를 입력하세요 :");
+				String id = sc.nextLine();
+				System.out.printf("로그인 PW를 입력하세요 :");
+				String pw = sc.nextLine();
+
+				int count = join(name, id, pw);
+
+				System.out.printf("%d번 회원으로 회원가입이 완료되었습니다.\n", count);
+
+			} else if (command.equals("member login")) {
+				System.out.println("== 로그인 ==");
+
+				System.out.printf("로그인 ID를 입력하세요 :");
+				String id = sc.nextLine();
+				System.out.printf("로그인 PW를 입력하세요 :");
+				String pw = sc.nextLine();
+
+				int count = login(id, pw);
+
+				if (count == -1) {
+					System.out.println("로그인 아이디 또는 페스워드가 잘못 입력되었습니다.");
+					continue;
+				}
+
+				System.out.printf("로그인 성공 %s님 반갑습니다.\n", members.get(count).name);
+
+			} else if (command.equals("member whoami")) {
+				System.out.println("== 로그인 정보 ==");
+
+				if (loginInformation < 0) {
+					System.out.println("현재 로그인 중인 회원이 존재하지 않습니다.");
+					continue;
+				}
+
+				System.out.printf("현재 로그인 아이디 : %s\n", members.get(loginInformation).id);
+				System.out.printf("이름 : %s\n", members.get(loginInformation).name);
+
+			} else if (command.equals("member logout")) {
+
+				if (loginInformation < 0) {
+					System.out.println("현재 로그인 중인 회원이 존재하지 않습니다.");
+					continue;
+				}
+
+				System.out.println("== 정상적으로 로그아웃 되었습니다. ==");
+				loginInformation = -1;
 
 			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
---
+++ src/com/sbs/example/easytextboard/Member.java
@@ -0,0 +1,8 @@
+package com.sbs.example.easytextboard;
+
+public class Member {
+	public int count;
+	public String name;
+	public String id;
+	public String pw;
+}
