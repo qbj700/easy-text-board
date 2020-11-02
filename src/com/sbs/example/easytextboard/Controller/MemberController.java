fea46816e72f2f6a0e7bd5fcab68902ff932a7ed
unknown <qbj700@gmail.com>
2020-11-02 09:48:29 +0900

    로그인,컨테이너,세션 도입

--- src/com/sbs/example/easytextboard/Controller/MemberController.java
+++ src/com/sbs/example/easytextboard/Controller/MemberController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.sbs.example.easytextboard.container.Container;
 import com.sbs.example.easytextboard.dto.Member;
 
 public class MemberController extends Controller {
@@ -53,7 +54,7 @@ public class MemberController extends Controller {
 
 			int loginIdMaxCount = 3;
 			int loginIdCount = 0;
-			boolean loginIdIsVaild = false;
+			boolean loginIdIsValid = false;
 
 			while (true) {
 				if (loginIdCount >= loginIdMaxCount) {
@@ -74,11 +75,11 @@ public class MemberController extends Controller {
 					continue;
 				}
 
-				loginIdIsVaild = true;
+				loginIdIsValid = true;
 				break;
 
 			}
-			if (loginIdIsVaild == false) {
+			if (loginIdIsValid == false) {
 				return;
 			}
 
@@ -108,7 +109,99 @@ public class MemberController extends Controller {
 
 			System.out.printf("%d번 회원이 생성되었습니다.\n", id);
 
+		} else if (command.equals("member login")) {
+			System.out.println("== 로그인 ==");
+
+			if (Container.session.isLogined()) {
+				System.out.println("이미 로그인 되었습니다.");
+				return;
+			}
+
+			String loginId = "";
+			String loginPw;
+
+			int loginIdMaxCount = 3;
+			int loginIdCount = 0;
+			boolean loginIdIsValid = false;
+
+			Member member = null;
+
+			while (true) {
+				if (loginIdCount >= loginIdMaxCount) {
+					System.out.println("로그인을 취소합니다.");
+					break;
+				}
+				System.out.printf("로그인 아이디 :");
+				loginId = sc.nextLine().trim();
+
+				if (loginId.length() == 0) {
+					System.out.println("입력된 값이 존재하지 않습니다.");
+					loginIdCount++;
+					continue;
+
+				}
+
+				member = getMemberByLoginId(loginId);
+
+				if (member == null) {
+					System.out.printf("존재하지 않는 로그인아이디 입니다.");
+					loginIdCount++;
+					continue;
+				}
+
+				loginIdIsValid = true;
+				break;
+
+			}
+			if (loginIdIsValid == false) {
+				return;
+			}
+
+			int loginPwMaxCount = 3;
+			int loginPwCount = 0;
+			boolean loginPwIsValid = false;
+
+			while (true) {
+				if (loginPwCount >= loginPwMaxCount) {
+					System.out.println("로그인을 취소합니다.");
+					break;
+				}
+				System.out.printf("로그인 비밀번호 :");
+				loginPw = sc.nextLine().trim();
+
+				if (loginPw.length() == 0) {
+					System.out.println("입력된 값이 존재하지 않습니다.");
+					loginPwCount++;
+					continue;
+
+				}
+
+				if (member.loginPw.equals(loginPw) == false) {
+					System.out.println("비밀번호가 일치하지 않습니다.");
+					loginPwCount++;
+					continue;
+				}
+
+				loginPwIsValid = true;
+				break;
+
+			}
+			if (loginPwIsValid == false) {
+				return;
+			}
+			System.out.printf("로그인 되었습니다. %s님 환영합니다.\n", member.name);
+
+			Container.session.loginedMemberId = member.memberId;
 		}
+	}
+
+	private Member getMemberByLoginId(String loginId) {
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return member;
+			}
+		}
+		return null;
 	}
 
 }
