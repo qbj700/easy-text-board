0875732f1ecd5e2d74d20c2ca19de283b7c89453
unknown <qbj700@gmail.com>
2020-11-02 12:58:45 +0900

    Dao구조 도입

---
+++ src/com/sbs/example/easytextboard/dao/ArticleDao.java
@@ -0,0 +1,86 @@
+package com.sbs.example.easytextboard.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sbs.example.easytextboard.dto.Article;
+
+public class ArticleDao {
+	private List<Article> articles;
+	private int lastArticleId;
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public int getLastArticleId() {
+		return lastArticleId;
+	}
+
+	public ArticleDao() {
+		lastArticleId = 0;
+		articles = new ArrayList<>();
+
+		for (int i = 0; i < 32; i++) {
+			add("제목" + (i + 1), "내용" + (i + 1), i % 2 == 0 ? 1 : 2);
+		}
+
+	}
+
+	// 게시물에 배열 번호 부여
+	public Article getArticle(int id) {
+		int index = getIndexById(id);
+
+		if (index == -1) {
+			return null;
+		}
+		return articles.get(index);
+	}
+
+	// 입력된 id가 articles.get(i).id 와 일치하는지 여부 확인
+	public int getIndexById(int id) {
+		for (int i = 0; i < articles.size(); i++) {
+			if (articles.get(i).id == id) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	// 게시물 삭제 함수
+	public void remove(int id) {
+		int index = getIndexById(id);
+
+		if (index == -1) {
+			return;
+		}
+		articles.remove(index);
+	}
+
+	// 게시물 생성 함수
+	public int add(String title, String body, int i) {
+
+		Article article = new Article(lastArticleId + 1, title, body, i);
+
+		articles.add(article);
+		lastArticleId = article.id;
+
+		return article.id;
+	}
+
+	// 게시물 수정 함수
+	public void modify(int inputedId, String title, String body) {
+		Article article = getArticle(inputedId);
+
+		article.title = title;
+		article.body = body;
+	}
+
+	public int getArticleSize() {
+		return articles.size();
+	}
+
+	public Article getArticleByIndex(int i) {
+		return articles.get(i);
+	}
+}
--- src/com/sbs/example/easytextboard/service/MemberService.java
+++ src/com/sbs/example/easytextboard/service/MemberService.java
@@ -1,58 +1,29 @@
 package com.sbs.example.easytextboard.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.sbs.example.easytextboard.container.Container;
+import com.sbs.example.easytextboard.dao.MemberDao;
 import com.sbs.example.easytextboard.dto.Member;
 
 public class MemberService {
-
-	private List<Member> members;
-	private int lastMemberId;
+	private MemberDao memberDao;
 
 	public MemberService() {
-		lastMemberId = 0;
-		members = new ArrayList<>();
-
-		for (int i = 1; i <= 3; i++) {
-			join("user" + i, "user" + i, "유저" + i);
-		}
-
+		memberDao = Container.memberDao;
 	}
 
 	// 회원 가입 함수
 	public int join(String loginId, String loginPw, String name) {
-
-		Member member = new Member();
-		member.memberId = lastMemberId + 1;
-		member.name = name;
-		member.loginId = loginId;
-		member.loginPw = loginPw;
-
-		members.add(member);
-		lastMemberId = member.memberId;
-
-		return member.memberId;
+		return memberDao.join(loginId, loginPw, name);
 	}
 
 	// 로그인아이디 유효성 테스트 함수
 	public boolean isJoinableLoginId(String loginId) {
-		for (Member member : members) {
-			if (member.loginId.equals(loginId)) {
-				return false;
-			}
-		}
-		return true;
+		return memberDao.isJoinableLoginId(loginId);
 	}
 
 	// 로그인 정보 확인 함수
 	public Member getMemberByLoginId(String loginId) {
-		for (Member member : members) {
-			if (member.loginId.equals(loginId)) {
-				return member;
-			}
-		}
-		return null;
+		return memberDao.getMemberByLoginId(loginId);
 	}
 
 }
